3d9dcf05fdb19db997b1e0a6124e46b8bf915f18
weiym97 <weiym97@163.com>
2022-10-20 10:25:36 +0100

    Add docstrings and comments in the class, and write DoseFn class

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -1,7 +1,76 @@
 #
 # Model class
 #
+import numpy as np
+class GaussConvFn():
+    """
+    This class represents the convolution of a delta function with Gaussian function
+    By Gaussian function we mean the pdf of Gaussian distribution.
+    The purpose of this function is build up a smooth function which is close to a delta function,
+    and enable the numerical solution of ODE using Runge-Kutta method.
+    Formally, Let us denote \rho(t)=A * \delta(t-t_center), where A is the magnitude
+    \omega(t) = \int \frac{1}{\sqrt{2\pi}\sigma}e^{-\frac{t^2}{2\sigma^2}},
+    then this class just represent \rho * \omega (t), where * is the convolution.
+    See the definition in https://en.wikipedia.org/wiki/Convolution for more information. 
+    
+    """
+    def __init__(self,center: float,magnitude: float,sigma=0.02):
+        """
+        params:
+        center: t_center
+        magnitude: magnitude in the delta function
+        sigma: sigma in the pdf of Gaussian distribution. 
+        
+        Note that sigma could not be too small, otherwise ODE solving might fail using Runge-Kutta method.
+            
+        """
+        self.center = center
+        self.magnitude = magnitude
+        self.sigma = sigma
 
+    def eval_at(self,x:float) -> float:
+        '''
+        return the value of this function at x 
+        '''
+        return self.magnitude / (2 * np.pi) ** 0.5 / self.sigma * np.exp(-(x-self.center)**2/(2 * self.sigma ** 2))
+
+class DoseFn():
+    """
+    This class represents the dose function in the ODE.
+    The dose function DOSE(t) should be a linear combination of several pseudo delta function(see GaussConvFn),
+    plus a constant value.
+    It represents consist of instantaneous doses of X ng of the drug at one or more time points,
+    or a steady application of X ng per hour over a given time period, or some combination.
+    
+    Building up an object needs to specify:
+    the stead application dose (constinput)
+    A list of instantaneous dosing time and quantity (centerpoints & magnitudes)
+    """
+    def __init__(self,constinput=0,centerpoints=None,magnitudes=None):
+        '''
+        params:
+        constinput: the steady dose, by default set to 0
+        centerpoints: time point of instantaneous doses, should be a list
+        magnitudes: amount of instantaneous doses, should be a list (length equal to centerpoints)
+        '''
+        self.constinput = constinput
+        self.deltainput = []
+        if centerpoints is not None:
+            if len(centerpoints) == len(magnitudes):
+                for i in range(len(centerpoints)):
+                    self.deltainput.append(GaussConvFn(centerpoints[i],magnitudes[i]))
+            else:
+                raise ValueError('The length of centerpoints and magnitudes list should be the same!')
+
+    def eval_at(self,x):
+        '''
+        Return the dose function value at x
+        '''
+        result = self.constinput
+        for i in range(len(self.deltainput)):
+            result += self.deltainput[i].eval_at(x)
+
+        return result
 class Model:
     """A Pharmokinetic (PK) model
 
--- pkmodel/solution.py
+++ pkmodel/solution.py
@@ -6,33 +6,31 @@
 import numpy as np
 import scipy.integrate
 
-
-
-def dose(t, X):
-    return X
-
-def rhs(t, y, Q_p1, V_c, V_p1, CL, X):
-    q_c, q_p1 = y
-    transition = Q_p1 * (q_c / V_c - q_p1 / V_p1)
-    dqc_dt = dose(t, X) - q_c / V_c * CL - transition
-    dqp1_dt = transition
-    return [dqc_dt, dqp1_dt]
 class Solution:
-    """A Pharmokinetic (PK) model solution
-
-    Parameters
-    ----------
-
-    value: numeric, optional
-        an example paramter
-
+    """A Pharmokinetic (PK) model solution,
+    running from t_0 to t_end with N time steps
     """
     def __init__(self, t_0=0, t_end=1, numsteps=1001):
+        '''
+        params:
+        t_0: start time
+        t_end: end time
+        numsteps: number of time steps
+        '''
         self.t_0 = t_0
         self.t_end = t_end
         self.numsteps = numsteps
 
-    def solve(self, model, args,y0):
+    def solve(self, model, args,y0:list):
+        '''
+        Solve the model ODE with initial value y0
+        params:
+        model: the ODE
+        args: value of parameters in the ODE,
+        y0: the initial value, length should be the same as ODE's variables.
+        return
+        sol: solution class returned by scipy.integrate.solve_ivp 
+        '''
         t_eval = np.linspace(self.t_0, self.t_end, self.numsteps)
         sol = scipy.integrate.solve_ivp(
             fun=lambda t, y: model(t, y, *args),
@@ -41,13 +39,73 @@ class Solution:
         )
         return sol
 
-
-
+def dose(t, X):
+    return X
+'''
+def dose(t,x, doze_time:list):
+    # for the continuous part,
+    # No matter t, we return x
+    if protocol == 'continuous':
+        return X
+    elif: 
+    # If the protocol is delta
+    #we only return x at specific time point
+        if t in doze_time:
+            return X
+        else:
+            return 0
+'''
+def rhs(t, y, Q_p1, V_c, V_p1, CL, X):
+    q_c, q_p1 = y
+    transition = Q_p1 * (q_c / V_c - q_p1 / V_p1)
+    dqc_dt = dose(t, X) - q_c / V_c * CL - transition
+    dqp1_dt = transition
+    return [dqc_dt, dqp1_dt]
+         
 
 # The purpose of the following code is just to play and learn with the solver.
 # It should never be run within the whole package
 
 if __name__ == '__main__':
+
+    ### Now we just need to write a small demo here
+    t_0 = 0.0
+    t_end = 1.0
+    numsteps = 101
+    t_eval = np.linspace(t_0,t_end,numsteps)
+    y0 = [0.0]
+    dosefn = DoseFn(0.0,[0.25,0.5,0.75],[2,1,1])
+    #dozefn = GaussConvFn(0.25,1)
+    xs = t_eval
+    ys = []
+    for x in xs:
+        ys.append(dosefn.eval_at(x))
+    import matplotlib.pyplot as plt
+    plt.plot(xs,np.array(ys))
+    plt.title('Input delta function')
+    plt.savefig('Input_delta_function_2.jpg')
+    plt.close()
+    '''
+    sol = scipy.integrate.solve_ivp(fun=lambda t,y:const_increase(t,y),
+                                    t_span=[t_0,t_end],
+                                    y0=y0,
+                                    t_eval=t_eval)
+    print(sol.y)
+    print('#########################')
+    '''
+    sol = scipy.integrate.solve_ivp(fun=lambda t,y:dozefn.eval_at(t),
+                                    t_span=[t_0,t_end],
+                                    y0=y0,
+                                    t_eval=t_eval)
+    print(sol.y.shape)
+    print(sol.t.shape)
+    plt.plot(sol.t,sol.y[0,:])
+    plt.title('Solution')
+    plt.savefig('Result_2.jpg')
+    plt.close()
+    #print(sol.t)
+    #print(sol.y)
+    '''
     model1_args = {
     'name': 'model1',
     'Q_p1': 1.0,
@@ -80,5 +138,7 @@ if __name__ == '__main__':
         #print(sol.y)
         print(sol.y.shape)
         print('######################')
+    '''
+
 
 
