876918fb61172877b1bcbe88451538884162804b
Lara Herriott <58111273+laraherriott@users.noreply.github.com>
2022-10-21 14:42:29 +0100

    Edit to tests

--- pkmodel/tests/test_solution.py
+++ pkmodel/tests/test_solution.py
@@ -63,14 +63,23 @@ class SolutionTest(unittest.TestCase):
         Tests model.equation() generates the correct number of equations which are then solved for.
         """
 
-        no_peripherals = 2
-        total_compartments = 1 + no_peripherals
-        ka = 9
-        if ka > 0:
-            total_compartments += 1
+        no_peripherals1 = 2
+        total_compartments1 = 1 + no_peripherals1
+        ka1 = 9
+        if ka1 > 0:
+            total_compartments1 += 1
 
-        model = Model(comp_num=no_peripherals, V_c=3, V_p=[3, 4], Q_p=[6, 9], CL=7, dose_comp=ka, constinput=5)
-        a = Solution(model, 0, 10)
+        no_peripherals2 = 0
+        total_compartments2 = 1 + no_peripherals2
+        ka2 = 0
+        if ka2 > 0:
+            total_compartments2 += 1
+
+        model1 = Model(comp_num=no_peripherals1, V_c=3, V_p=[3, 4], Q_p=[6, 9], CL=7, dose_comp=ka1, constinput=5)
+        model2 = Model(comp_num=no_peripherals2, V_c=3, V_p=[], Q_p=[], CL=7, dose_comp=ka2, constinput=0, centerpoints=[1, 2, 3, 4], magnitudes=[1, 2, 3, 4])
+        models = [model1, model2]
+        a = Solution(models, 0, 10)
         all_solutions, all_specifications = a.generate_solutions()
 
-        self.assertEqual(total_compartments, len(all_solutions[0].y))
+        self.assertEqual(total_compartments1, len(all_solutions[0].y))
+        self.assertEqual(total_compartments2, len(all_solutions[1].y))
